14602be9ece5bb7c029d93894df49ef6fbdddec8
Ajay Kumar Kumawat <ajaykumarkumawat608@gmail.com>
2026-02-15 04:56:30 +0000

    study about data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId =14453
+let accountEmail ="ajaymanit@gmail.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+/*
+prefer not use of var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
