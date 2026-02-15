2823cf5b262c5f5393e8fb91cd63cb89b2dc0813
Ajay Kumar Kumawat <ajaykumarkumawat608@gmail.com>
2026-02-15 18:56:10 +0000

    complete data types and conversionoperation

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,21 @@
 // alert(3+3);//we are using nodejs , not browser 
 console.log(3+3)
 console.log("Ajay")
+let name = "Ajay"
+let age = 20
+let isLoggedIn = false
+let state;
+//---------primitive data types------------
+// number => 2 to power 53(range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique(check unique or not --most use in react)
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Ajay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ajay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+//***************Operation*************** 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); when string first come then all treat like string 
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");//if first number is come then first add then work like string concatination
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); change operator
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
