c12644f1add1ec6106830b768a338b1a7fc235ec
ak_kumawat <ajaykumarkumawat608@gmail.com>
2026-02-19 19:01:24 +0530

    study about how to iife work

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -8,3 +8,48 @@ const user = {
     }
 
 }
+
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+//+++++++++++++++++++Arrow function ++++++++++++++++++++++
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+//const addTwo = (num1, num2) =>  num1 + num2
+
+ //const addTwo = (num1, num2) => ( num1 + num2 )
+
+ const addTwo = (num1, num2) => ({username: "hitesh"})//for return object
+
+
+//console.log(addTwo(3, 4))
+
+
+const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
